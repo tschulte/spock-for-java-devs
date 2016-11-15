486264c2923767ea9987d5afe3fc4e6f018ffdaa
Tobias Schulte <Tobias.Schulte@kratzer-automation.com>
2016-11-15 13:18:54 +0100

    some more content for the spock section

---
+++ src/test/groovy/de/gliderpilot/spock/MathSpec.groovy
@@ -0,0 +1,18 @@
+package de.gliderpilot.spock
+
+import spock.lang.*
+
+class MathSpec extends Specification {
+
+    @Unroll
+    def "maximum of #a and #b is #c"() {
+        expect:
+        Math.max(a, b) == c
+
+        where:
+        a | b | c
+        1 | 3 | 3
+        7 | 4 | 7
+        0 | 0 | 0
+    }
+}
